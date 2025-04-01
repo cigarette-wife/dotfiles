802303999e345a4a47d3d0baf9efbae617fbb7f7
Maxine <maxine@mxhzl.com>
2025-04-01 13:59:25 -0400

    Update config.fish
    Remove unnecessary config variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,12 +5,9 @@ set -gx TERM xterm-256color
 set -xg LS_COLORS "bxExCxDxfxegedabagacaf"
 set -xg LS_COLORS "di=31:ln=1;34:so=1;32:pi=1;33:ex=35:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;45"
 
-set -x -U GOPATH $HOME/go
-set -xg PATH $GOPATH/bin $PATH
 set -xg PATH $HOME/.cargo/bin $PATH
 set -xg PATH $HOME/.asdf/bin $PATH
 set -xg PATH $HOME/.asdf/shims $PATH
-set -xg PATH /opt/nvim-linux64/bin $PATH
 set -xg PATH $HOME/.bin $PATH
 set -xg PATH $HOME/.local/bin $PATH
 
