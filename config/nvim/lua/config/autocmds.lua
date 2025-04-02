4fcba44595554fe3b38b8127e69ac7e2fdbb0b98
Maxine <maxine@mxhzl.com>
2025-04-02 17:12:48 -0400

    (hopefully) better rails support in nvim

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set filetype yaml for eruby-yaml
+vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+  pattern = "*.yml",
+  command = "setlocal filetype=yaml",
+})
+-- Map the custom gf function to gf in Rails files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "eruby", "html.erb" },
+  callback = function()
+    vim.keymap.set("n", "gf", ":Railsgf<cr>", { buffer = true, noremap = true, silent = true })
+  end,
+})
