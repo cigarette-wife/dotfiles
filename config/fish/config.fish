d97dd31a76b72f0a629de38f78a27625dd3a8cef
Maxine <maxine@mxhzl.com>
2025-04-02 16:38:05 -0400

    Switches from asdf to  mise

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,12 +2,10 @@ set fish_greeting ""
 
 set -gx CLICOLOR 1
 set -gx TERM xterm-256color
-set -xg LS_COLORS "bxExCxDxfxegedabagacaf"
+set -xg LS_COLORS bxExCxDxfxegedabagacaf
 set -xg LS_COLORS "di=31:ln=1;34:so=1;32:pi=1;33:ex=35:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;45"
 
 set -xg PATH $HOME/.cargo/bin $PATH
-set -xg PATH $HOME/.asdf/bin $PATH
-set -xg PATH $HOME/.asdf/shims $PATH
 set -xg PATH $HOME/.bin $PATH
 set -xg PATH $HOME/.local/bin $PATH
 
@@ -17,4 +15,3 @@ set -xg EDITOR $VISUAL
 if test -e $HOME/.config/fish/config.fish.local
     source $HOME/.config/fish/config.fish.local
 end
-
